4cf7f91a377cf8e522ff7f7baa88e745f7303841
Andy Jones <andytjones@icloud.com>
2019-12-29 21:30:18 +0000

    Feature - Top_up method now allows adding money to card.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,20 @@ require 'oystercard'
 
 describe Oystercard do
 
+  let(:card) { described_class.new }
+
   it 'has a zero balance at start' do
-    expect(subject.balance).to eq(0)
+    expect(card.balance).to eq(0)
+  end
+
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up balance' do
+      expect { card.top_up(1) }.to change{ card.balance }.by 1
+    end
+
   end
 
 end
